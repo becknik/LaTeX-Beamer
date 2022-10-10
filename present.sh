7318e357629992967901e51f15d99556f49ae85e
jarnnk <jannikb@posteo.de>
2022-10-10 02:21:42 +0200

    Added simple pdfpc script

---
+++ present.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This simple command provides a working presentation solution for my laptop:
+pdfpc --wayland-workaround --notes=right --page-transition=fade:0.5 ./presentation.pdf
+
+
+# Available animations from "man pdfpc": 
+#	blinds, box, cover, dissolve, fade, glitter (except the diagonal one), push, split, uncover, and wipe
+# Formatting form "man pdfpc":
+#	--page-transition=type:duration:angle:alignment:direction.
