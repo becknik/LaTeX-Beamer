8158d496038c16c11d4459cc20e4eafec95e965e
jarnnk <jannikb@posteo.de>
2022-11-02 22:15:19 +0100

    Enabled default option without LaTeX-notes in pdfpc 'script'

--- present.sh
+++ present.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # This simple command provides a working presentation solution for my laptop:
-pdfpc --wayland-workaround --notes=right --page-transition=fade:0.5 ./presentation.pdf
+pdfpc --wayland-workaround --notes=none --page-transition=fade:0.5 ./presentation.pdf
+# With beamer notes enabled:
+#pdfpc --wayland-workaround --notes=right --page-transition=fade:0.5 ./presentation.pdf
 
 
 # Available animations from "man pdfpc": 
